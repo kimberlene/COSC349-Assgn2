7ea3e4bc4c2d965238115d2d7b3b2e013c59fdf0
Kimberlene Sharma <kimberlenes@gmail.com>
2019-10-04 22:12:44 +1300

    Added database setup file from assignment 1

---
+++ setup-database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE notes (
+  note varchar(20) NOT NULL
+);
+
+INSERT INTO notes VALUES ('example note');
+INSERT INTO notes VALUES ('example note 2');
+INSERT INTO notes VALUES ('example note 3');
+
+
